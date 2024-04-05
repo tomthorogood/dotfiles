e265b6b60860d08d665888f8d04954c4a76ea02e
Tom Thorogood <tomthorogood@github.com>
2024-04-05 10:39:44 -0700

    Add basic codespaces setup

--- codespaces/setup.sh
+++ codespaces/setup.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 HERE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}" )" &> /dev/null && pwd )
+
+${HERE}/../git/configure.sh
+${HERE}/../starship/install.sh
+${HERE}/../vim/configure.sh
